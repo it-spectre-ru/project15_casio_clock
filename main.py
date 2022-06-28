680e19a276b2d8f21b485306ad0b04426dbfa731
s <s>
2022-06-28 18:29:59 +0500

    fix

--- main.py
+++ main.py
@@ -1,104 +1,102 @@
 import csv
 import json
-import time
 import os
+import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 
 def get_all_pages():
-	headers = {
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
-	}
+    headers = {
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+    }
 
-	# r = requests.get(url='https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/', headers=headers)
+    r = requests.get(url="https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/", headers=headers)
 
-	# if not os.path.exists('data'):
-	# 	os.mkdir('data')
+    if not os.path.exists("data"):
+        os.mkdir("data")
 
-	# with open('data/page_1.html', 'w') as file:
-	# 	file.write(r.text)
+    with open("data/page_1.html", "w") as file:
+        file.write(r.text)
 
-	with open('data/page_1.html') as file:
-		src = file.read()
+    with open("data/page_1.html") as file:
+        src = file.read()
 
-	soup = BeautifulSoup(src, 'lxml')
-	pages_count = int(soup.find('div', class_='bx-pagination-container').find_all('a')[-2].text)
-	
-	for i in range(1, pages_count + 1):
-		url = f'https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/?PAGEN_1={i}'
-		
-		r = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(src, "lxml")
+    pages_count = int(soup.find("div", class_="bx-pagination-container").find_all("a")[-2].text)
 
-		with open(f'data/page_{i}.html', 'w') as file:
-			file.write(r.text)
+    for i in range(1, pages_count + 1):
+        url = f"https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/?PAGEN_1={i}"
 
-		time.sleep(2)
-	
-	return pages_count + 1
+        r = requests.get(url=url, headers=headers)
 
+        with open(f"data/page_{i}.html", "w") as file:
+            file.write(r.text)
+
+        time.sleep(2)
+
+    return pages_count + 1
 
 
 def collect_data(pages_count):
-	cur_date = datetime.now().strftime("%d_%m_%Y")
+    cur_date = datetime.now().strftime("%d_%m_%Y")
 
-	with open(f'data_{cur_date}.csv', 'w') as file:
-		writer = csv.writer(file)
+    with open(f"data_{cur_date}.csv", "w") as file:
+        writer = csv.writer(file)
 
-		writer.writerow(
-			(
-				"Артикул",
-				"Ссылка",
-				"Цена"
-			)
-		)
+        writer.writerow(
+            (
+                "Артикул",
+                "Ссылка",
+                "Цена"
+            )
+        )
 
-	data = []
-	for page in range(1, pages_count):
-		with open(f'data/page_{page}.html') as file:
-			src = file.read()
+    data = []
+    for page in range(1, pages_count):
+        with open(f"data/page_{page}.html") as file:
+            src = file.read()
 
-		soup = BeautifulSoup(src, 'lxml')
-		items_cards = soup.find_all('a', class_='product-item__link')
+        soup = BeautifulSoup(src, "lxml")
+        items_cards = soup.find_all("a", class_="product-item__link")
 
-		for item in items_cards:
-			product_article = item.find('p', class_='product-item__articul').text.strip()
-			product_price = item.find('p', class_='product-item__price').text
-			product_url = f'https://shop.casio.ru{item.get("href")}'
+        for item in items_cards:
+            product_article = item.find("p", class_="product-item__articul").text.strip()
+            product_price = item.find("p", class_="product-item__price").text.lstrip("руб. ")
+            product_url = f'https://shop.casio.ru{item.get("href")}'
 
-			#print(f'Article: {product_article} - Price: {product_price} - URL: {product_url}')
+            # print(f"Article: {product_article} - Price: {product_price} - URL: {product_url}")
 
-			data.append(
-				{
-					"product_article": product_article,
-					"product_url": product_url,
-					"product_price": product_price
-				}
-			)
+            data.append(
+                {
+                    "product_article": product_article,
+                    "product_url": product_url,
+                    "product_price": product_price
+                }
+            )
 
-			with open(f'data_{cur_date}.csv', 'a') as file:
-				writer = csv.writer(file)
+            with open(f"data_{cur_date}.csv", "a") as file:
+                writer = csv.writer(file)
 
-			writer.writerow(
-				(
-					product_article,
-					product_url,
-					product_price
-				)
-			)
+                writer.writerow(
+                    (
+                        product_article,
+                        product_url,
+                        product_price
+                    )
+                )
 
-		print(f"[INFO] {page}")
+        print(f"[INFO] Обработана страница {page}/5")
 
-	with open(f'data_{cur_date}.json', 'a') as file:
-		json.dump(data, file, indent=4, ensure_ascii=False)
-
-
+    with open(f"data_{cur_date}.json", "a") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
-	pages_count = get_all_pages()
-	collect_data(pages_count=pages_count)
+    pages_count = get_all_pages()
+    collect_data(pages_count=pages_count)
+
 
 if __name__ == '__main__':
-	main()
+    main()
