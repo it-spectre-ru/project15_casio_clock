8f336d5fbda9a65bb8152c0ca673271d82cbdbb7
s <s>
2022-06-28 17:07:12 +0500

    get html clocks

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-l = []
+import os
+import requests
+from bs4 import BeautifulSoup
 
-for i in range(1, 100):
-	if (i%2 != 0):
-		l.append(i)
+def get_all_pages():
+	headers = {
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+	}
 
-print(l)
+	r = requests.get(url='https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/', headers=headers)
+
+	if not os.path.exists('data'):
+		os.mkdir('data')
+
+	with open('data/page_1.html', 'w') as file:
+		file.write(r.text)
+
+
+def main():
+	get_all_pages()
+
+if __name__ == '__main__':
+	main()
