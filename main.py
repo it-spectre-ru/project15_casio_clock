7338562b2f97e2dce76fd299ddb07816273ad267
s <s>
2022-06-28 17:16:09 +0500

    get all clock URLS from paginationget all clock URLS from pagination

--- main.py
+++ main.py
@@ -7,13 +7,24 @@ def get_all_pages():
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
 	}
 
-	r = requests.get(url='https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/', headers=headers)
+	# r = requests.get(url='https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/', headers=headers)
 
-	if not os.path.exists('data'):
-		os.mkdir('data')
+	# if not os.path.exists('data'):
+	# 	os.mkdir('data')
 
-	with open('data/page_1.html', 'w') as file:
-		file.write(r.text)
+	# with open('data/page_1.html', 'w') as file:
+	# 	file.write(r.text)
+
+	with open('data/page_1.html') as file:
+		src = file.read()
+
+	soup = BeautifulSoup(src, 'lxml')
+	pages_count = int(soup.find('div', class_='bx-pagination-container').find_all('a')[-2].text)
+	
+	for i in range(1, pages_count + 1):
+		url = f'https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/?PAGEN_1={i}'
+		print(url)
+
 
 
 def main():
